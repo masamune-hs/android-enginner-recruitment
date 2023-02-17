3a7f29b9581b15e59128257437220d9446dbd9f2
masamune_hs <life.scrolling.hw@gmail.com>
2023-02-18 03:38:48 +0900

    Revert "add custom header"
    This reverts commit 372b745f188a5b69793fee0c76a1370fe1f3fa7f.

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
@@ -1,22 +1,20 @@
 package com.oishikenko.android.recruitment.feature.list
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.consumedWindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -30,23 +28,10 @@ fun RecipeListScreen(
     val cookingRecords by viewModel.cookingRecords.collectAsStateWithLifecycle()
     Scaffold(
         topBar = {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(
-                    space = 8.dp,
-                    alignment = Alignment.CenterHorizontally
-                ),
-                verticalAlignment = Alignment.CenterVertically
+            TopAppBar(
+                modifier = Modifier,
             ) {
-                Text(
-                    text = stringResource(id = R.string.cooking_records_title),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 24.sp
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.header_icon),
-                    contentDescription = "header_icon"
-                )
+                Text(text = stringResource(id = R.string.cooking_records_title))
             }
         }
     ) { innerPadding ->
@@ -65,7 +50,7 @@ fun RecipeListScreen(
 
 @Preview
 @Composable
-fun PreviewRecipeListScreen() {
+fun PreviewRecipeListScreen(){
     MaterialTheme {
         RecipeListScreen()
     }
