7acd5e668ac42387a6d2de55341cd8d77eaca8b1
masamune_hs <life.scrolling.hw@gmail.com>
2023-02-21 18:37:41 +0900

    [WIP] recipe detail layout

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeDetail.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeDetail.kt
@@ -1,5 +1,7 @@
 package com.oishikenko.android.recruitment.feature.list
 
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -8,13 +10,22 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.oishikenko.android.recruitment.data.model.CookingRecord
 import com.oishikenko.android.recruitment.feature.R
+
+enum class RecipeType(val recipeType: String) {
+    MAIN_DISH("main_dish"),
+    SIDE_DISH("side_dish"),
+    SOUP("SOUP")
+}
 
 @Composable
 fun RecipeDetail(cookingRecord: CookingRecord) {
@@ -33,33 +44,56 @@ fun RecipeDetail(cookingRecord: CookingRecord) {
         }
     ) { innerPadding ->
         Column(Modifier.padding(innerPadding)) {
-            Box(modifier = Modifier
-                .fillMaxWidth()
-                .height(360.dp)
-                .background(color = colorResource(id = R.color.recorded_at_text_color)
-                )) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(360.dp)
+                    .background(
+                        color = colorResource(id = R.color.recorded_at_text_color)
+                    )
+            ) {
                 AsyncImage(
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(360.dp)
-                        .background(color = colorResource(id = R.color.recorded_at_text_color))
-                    ,
+                        .background(color = colorResource(id = R.color.recorded_at_text_color)),
                     model = cookingRecord.imageUrl,
                     contentDescription = "thumbnail"
                 )
-                Text(
-                    modifier = Modifier.align(Alignment.BottomEnd),
-                    text = cookingRecord.recipeType
+                Image(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.BottomEnd),
+                    painter = setupRecipeTypeLabel(recipeType = cookingRecord.recipeType),
+                    contentDescription = "recipe_type"
                 )
             }
-            Text(modifier = Modifier.fillMaxWidth(), text = cookingRecord.comment)
             Text(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, top = 8.dp)
+                    .fillMaxWidth(),
+                text = cookingRecord.comment,
+                fontSize = 16.sp
+            )
+            Text(
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp, top = 4.dp, bottom = 8.dp)
                     .align(Alignment.End),
-                text = cookingRecord.recordedAt
+                text = cookingRecord.recordedAt,
+                fontSize = 14.sp,
+                color = colorResource(id = R.color.recorded_at_text_color)
             )
         }
+    }
+}
+
+@Composable
+fun setupRecipeTypeLabel(recipeType: String): Painter {
+    return when (recipeType) {
+        RecipeType.MAIN_DISH.recipeType -> painterResource(id = R.drawable.label_main_dish)
+        RecipeType.SIDE_DISH.recipeType -> painterResource(id = R.drawable.label_side_dish)
+        RecipeType.SOUP.recipeType -> painterResource(id = R.drawable.label_soup)
+        else -> painterResource(id = 0)
     }
 }
 
